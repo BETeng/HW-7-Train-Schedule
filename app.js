42a43008ef656d40c73f5ae628774f7e1c63da1d
BETeng <brian7.teng@gmail.com>
2019-05-08 03:08:11 -0500

    finished js logic

--- app.js
+++ app.js
@@ -1,9 +1,73 @@
-// js for train schedule
 
-configure firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyDhCrYVVdR5KLqrUZn2qqyJdNIGGau4s2c",
+  authDomain: "fir-demo-6dcaf.firebaseapp.com",
+  databaseURL: "https://fir-demo-6dcaf.firebaseio.com",
+  projectId: "fir-demo-6dcaf",
+  storageBucket: "fir-demo-6dcaf.appspot.com",
+  messagingSenderId: "254292077950",
+  appId: "1:254292077950:web:30513dc145367b62"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var clickCounter = 0;
+var database = firebase.database();
 
-create a td for each part of 5 data sets
+$('.btn').on('click', function (event) {
+  event.preventDefault();
+  var trainName = $('#data-train-name').val();
+  var destination = $('#data-destination').val();
+  var firstTrain = $('#data-first-train').val();
+  var frequency = $('#data-frequency').val();
 
-appdend the data to the row
+  var newTrain = {
+    name: trainName,
+    destination: destination,
+    first: firstTrain,
+    freq: frequency
+  };
 
-append the row to html table
+  database.ref().push(newTrain);
+  $('#data-train-name').val('');
+  $('#data-destination').val('');
+  $('#data-first-train').val('');
+  $('#data-frequency').val('');
+})
+
+
+
+database.ref().on('child_added', function (childSnapshot) {
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().destination
+  var firstTrain = childSnapshot.val().first
+  var frequency = childSnapshot.val().freq
+
+  var nameCell = $('<td>').text(trainName);
+  var destCell = $('<td>').text(destination);
+  // var firstCell = $('<td>').text(firstTrain);
+  var freqCell = $('<td>').text(frequency);
+
+
+  var firstTrainTime = moment(firstTrain, 'hh:mm');
+  console.log(firstTrainTime)
+
+  var now = moment().format('HH:mm');
+  console.log('now', now)
+
+  var timeDiff = moment().diff(moment(firstTrainTime), 'minutes');
+  console.log('timeDiff', timeDiff);
+
+  var remainder = timeDiff % frequency;
+  console.log(remainder);
+
+  var minutesAway = frequency - remainder;
+  var minCell = $('<td>').text(minutesAway);
+  console.log('minutesAway', minutesAway);
+
+  var nextArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
+  console.log(nextArrival);
+  var nextCell = $('<td>').text(nextArrival);
+
+  var newRow = $('<tr>').append(nameCell, destCell, freqCell, nextCell, minCell);
+  $('.table').append(newRow)
+})
